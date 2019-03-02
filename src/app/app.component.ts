63ba9b7bdcbf077df5b463be15823d3ee07b203c
Jack Alterman <jackalterman@gmail.com>
2019-03-02 14:42:28 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'Here is a list of issues for the Angular project';
+
+      // specify the columns
+      columnDefs = [
+        {headerName: "Issue Number", field: "number", resizable:true, sortable:true, width: 120 },
+        {headerName: "Date Created", field: "created_at", filter: true, resizable:true,sortable:true},
+        //{headerName: "URL", field: "url",  filter: true, resizable:true, sortable:true, width: 400},
+        {headerName: "Title", field: "title", filter: true, resizable:true, sortable:true, width: 400},
+        {headerName: "Body", field: "body", filter: true, width: 900, resizable:true,sortable:true},      
+        {headerName: "User", field: "user.login", filter: true, resizable:true,sortable:true}
+      ];
+
+      rowData: any;
+
+      constructor(private http: HttpClient) {
+}
+
+
+ngOnInit() {
+  var todayDate = new Date();
+  todayDate.setDate(todayDate.getDate()-7);
+  var d  = todayDate.toISOString();
+  this.rowData = this.http.get('https://api.github.com/repos/angular/angular/issues?since='+d + '&per_page=100')
+}
+}
